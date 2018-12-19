d02159fb15464775501edb467c6682a6abc2b874
Liz Spangler <lizspan@gmail.com>
2018-12-19 16:24:06 -0500

    integration test

--- demo-app-repository-mysql/src/test/java/org/example/repository/StopPaymentIntegration.java
+++ demo-app-repository-mysql/src/test/java/org/example/repository/StopPaymentIntegration.java
@@ -14,22 +14,22 @@ import org.junit.Test;
 
 public class StopPaymentIntegration {
 
-	private TestMySqlStopPaymentRepository stopPaymentRepository = new TestMySqlStopPaymentRepository();
+	private TestMySqlStopPaymentRepository testStopPaymentRepository = new TestMySqlStopPaymentRepository();
 
 	@Before
 	public void setup() {
 		// Clear test data using test impl of the repository, before each test
-		stopPaymentRepository.clearAllStopPayments();
+		testStopPaymentRepository.clearAllStopPayments();
 	}
 
 	@Test
 	public void shouldSaveAndRetrieveStopPayment() {
 		// Populate test data
 		StopPayment stopPaymentForSave = makeStopPayment();
-		stopPaymentRepository.addStopPayment(stopPaymentForSave);
+		testStopPaymentRepository.addStopPayment(stopPaymentForSave);
 
 		// Retrieve and check
-		List<StopPayment> retrievedStopPayments = stopPaymentRepository.getAllStopPayments();
+		List<StopPayment> retrievedStopPayments = testStopPaymentRepository.getAllStopPayments();
 		if (retrievedStopPayments.isEmpty()) {
 			fail("No stop payment retrieved");
 		} else {
@@ -43,18 +43,18 @@ public class StopPaymentIntegration {
 	public void shouldSaveAndRetrieveMultipleStopPayments() {
 		// Populate test data
 		StopPayment stopPaymentForSave1 = makeStopPayment();
-		stopPaymentRepository.addStopPayment(stopPaymentForSave1);
+		testStopPaymentRepository.addStopPayment(stopPaymentForSave1);
 
 		StopPayment stopPaymentForSave2 = makeStopPayment();
 		stopPaymentForSave2.setDebitCardNumber("0000111100001112");
-		stopPaymentRepository.addStopPayment(stopPaymentForSave2);
+		testStopPaymentRepository.addStopPayment(stopPaymentForSave2);
 
 		StopPayment stopPaymentForSave3 = makeStopPayment();
 		stopPaymentForSave3.setDebitCardNumber("0000111100001113");
-		stopPaymentRepository.addStopPayment(stopPaymentForSave3);
+		testStopPaymentRepository.addStopPayment(stopPaymentForSave3);
 
 		// Retrieve and check
-		List<StopPayment> retrievedStopPayments = stopPaymentRepository.getAllStopPayments();
+		List<StopPayment> retrievedStopPayments = testStopPaymentRepository.getAllStopPayments();
 		if (retrievedStopPayments.isEmpty()) {
 			fail("No stop payment retrieved");
 		} else {
